efb4b89866b97ed503ea8b72210ec84b60a002ab
msaf1980 <msaf1980@gmail.com>
2021-08-15 17:37:42 +0500

    build: fix test options and dependecies

--- dependecies.cmake
+++ dependecies.cmake
@@ -12,21 +12,21 @@ set(CMAKE_REQUIRED_LIBRARIES Threads::Threads) # required libs for check_functio
 if (BUILD_TESTING)
     check_symbol_exists(pthread_barrier_init pthread.h HAVE_PTHREAD_BARRIER) # for MacOS
     check_symbol_exists(pthread_spin_init pthread.h HAVE_PTHREAD_SPIN) # for MacOS
+
+    if ("${BUILD_DEPS}" STREQUAL "off")
+        # Use libraries from OS
+    else()
+        # Init cget
+        cget_init(CGET_INIT_CMD "${BUILD_DEPS}" "${CGET_DIR}")
+        cget_install(CGET_INSTALL_CMD  "${CMAKE_SOURCE_DIR}/requirements.txt" "${CGET_DIR}")
+
+        if (NOT DEFINED CMAKE_PREFIX_PATH)
+            set(CMAKE_PREFIX_PATH "${CGET_DIR}/cget")
+        endif()
+    endif() # BUILD_DEPS
+
+    pkg_check_modules(LIBCONCURRENT REQUIRED libconcurrent>=0.0.1)
+    include_directories(${LIBCONCURRENT_INCLUDE_DIRS})
+    link_directories(${LIBCONCURRENT_LIBRARY_DIRS})
+    link_libraries(concurrent)
 endif()
-
-if ("${BUILD_DEPS}" STREQUAL "off")
-    # Use libraries from OS
-else()
-    # Init cget
-    cget_init(CGET_INIT_CMD "${BUILD_DEPS}" "${CGET_DIR}")
-    cget_install(CGET_INSTALL_CMD  "${CMAKE_SOURCE_DIR}/requirements.txt" "${CGET_DIR}")
-
-    if (NOT DEFINED CMAKE_PREFIX_PATH)
-        set(CMAKE_PREFIX_PATH "${CGET_DIR}/cget")
-    endif()
-endif() # BUILD_DEPS
-
-pkg_check_modules(LIBCONCURRENT REQUIRED libconcurrent>=0.0.1)
-include_directories(${LIBCONCURRENT_INCLUDE_DIRS})
-link_directories(${LIBCONCURRENT_LIBRARY_DIRS})
-link_libraries(concurrent)
